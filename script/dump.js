bb67589ed54ba70c72ad89210cba1d580e80a5f2
Lars Thorup <lars@zealake.com>
2017-04-01 15:13:41 +0200

    dump script

---
+++ script/dump.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const sheetReader = require('..');
+
+const oldPath = process.argv[2];
+
+const oldData = sheetReader.readFile(oldPath, {excludeMetadata: true});
+
+const oldJsonPath = path.join(path.dirname(oldPath), path.basename(oldPath, path.extname(oldPath)) + '.json');
+
+const oldJson = JSON.stringify(oldData, null, 2);
+
+fs.writeFileSync(oldJsonPath, oldJson);
